d56dadbbfe5dc3fdabefdb4ba70e9355226090d6
Rahul Gupta <guptarahul07@gmail.com>
2026-05-03 15:41:07 +0530

    updated file to remove log

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -2,9 +2,6 @@ import { initializeApp } from "firebase/app"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
-console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY)
-
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
